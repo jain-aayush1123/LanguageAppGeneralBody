5bae057ceb1d4858920624bebbfb9568f163cd9c
awsm1123 <jaayushmax@gmail.com>
2019-11-19 16:21:50 +0530

    add duo nav drawer

---
+++ app/src/main/java/awsm/awsmizng/u/languageappgeneralbody/BlankFragment.java
@@ -0,0 +1,24 @@
+package awsm.awsmizng.u.languageappgeneralbody;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        return view;
+    }
+}
